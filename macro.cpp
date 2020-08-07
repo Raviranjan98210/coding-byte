eb6cd962aee68a3b784fe7e7b8983ac1cca14e4b
Ravi Ranjan Kumar <ravi.kumar@mounty.co>
2020-08-07 19:29:49 +0530

    implementing micro to calculate area of circle

---
+++ macro.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14
+
+int main()
+{
+    int r;
+    cout << "Enter the radius of circle" << endl;
+    cin >> r;
+
+    cout << PI * r * r << endl;
+}
